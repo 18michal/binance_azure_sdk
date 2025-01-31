0c7165bbbed9afe78b5d174873d2ef114410eaa0
18michal <18janowski@gmail.com>
2025-01-31 20:39:42 +0100

    added: get secret keys classes and execution

---
+++ src/helpers.py
@@ -0,0 +1,27 @@
+import logging
+
+
+def configure_logger(name: str) -> logging.Logger:
+    """
+    Configures and returns a logger with the specified name.
+
+    Args:
+        name (str): The name of the logger.
+
+    Returns:
+        logging.Logger: Configured logger instance.
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    if not logger.hasHandlers():  # Avoid adding multiple handlers
+        logger.addHandler(console_handler)
+
+    return logger
